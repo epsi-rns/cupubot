b350eda052bd333b70219f833a6d64477590a29e
epsi <epsi.rns@gmail.com>
2018-01-29 05:55:05 +0700

    loop: bash: messages.bash: add tasks

--- loop/bash/messages.bash
+++ loop/bash/messages.bash
@@ -8,7 +8,11 @@ operations:
  general
    -v, --version    display version information
    -h, --help       display help information
-
+   --observe        show JSON output of getUpdates
+   --reply          reply all messages
+   --new-member     greet new member
+   --logger-text    log chat conversation
+   --logger-html    log chat conversation
 EOF
 }
 
